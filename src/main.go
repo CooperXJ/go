69f2f6e55f788ff3817226da942cf272a500336b
Cooper <1789023580@qq.com>
2020-09-28 18:28:41 +0800

    第二次提交

--- src/main.go
+++ src/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "fmt"
+//import "fmt"
+
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 
-	fmt.Println("Hello GO")
+	var i = 10
+	i = 11
 
-	fmt.Println("Hello aaaa")
+	var c byte = 255
 
-	fmt.Println("Hello aaaa")
+	fmt.Println(i, c)
+
+	//输出查看变量的类型
+	fmt.Printf("i =  %T ", i)
+
+	fmt.Println()
+
+	//输出查看变量占用的字节数
+	fmt.Printf("i 的字节数为 %d ",unsafe.Sizeof(i))
 
 }
